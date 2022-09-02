7cdc87ff9e7e6e387aad22802cc97fb15c14c26c
rvallejosbarria <rvallejosbarria@gmail.com>
2022-09-01 22:17:57 -0400

    Fully implemented Commands API

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -27,7 +27,7 @@ namespace CommandAPI.Controllers
       return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}", Name = "GetCommandById")]
     public ActionResult<CommandReadDto> GetCommandById(int id)
     {
       var commandItem = _repository.GetCommandById(id);
@@ -36,6 +36,52 @@ namespace CommandAPI.Controllers
         return NotFound();
       }
       return Ok(_mapper.Map<CommandReadDto>(commandItem));
+    }
+
+    [HttpPost]
+    public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+    {
+      var commandModel = _mapper.Map<Command>(commandCreateDto);
+      _repository.CreateCommand(commandModel);
+      _repository.SaveChanges();
+
+      var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
+      return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+    {
+      var commandModelFromRepo = _repository.GetCommandById(id);
+      if (commandModelFromRepo == null)
+      {
+        return NotFound();
+      }
+
+      _mapper.Map(commandUpdateDto, commandModelFromRepo);
+
+      _repository.UpdateCommand(commandModelFromRepo);
+
+      _repository.SaveChanges();
+
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteCommand(int id)
+    {
+      var commandModelFromRepo = _repository.GetCommandById(id);
+      if (commandModelFromRepo == null)
+      {
+        return NotFound();
+      }
+
+      _repository.DeleteCommand(commandModelFromRepo);
+
+      _repository.SaveChanges();
+
+      return NoContent();
     }
   }
 }
--- src/CommandAPI/Profiles/CommandsProfile.cs
+++ src/CommandAPI/Profiles/CommandsProfile.cs
@@ -9,6 +9,8 @@ namespace CommandAPI.Profiles
     public CommandsProfile()
     {
       CreateMap<Command, CommandReadDto>();
+      CreateMap<CommandCreateDto, Command>();
+      CreateMap<CommandUpdateDto, Command>();
     }
   }
 }
