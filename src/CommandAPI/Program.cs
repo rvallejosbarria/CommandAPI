1dc9b06a18d2c465ea178954da67561c1b25affc
rvallejosbarria <rvallejosbarria@gmail.com>
2024-11-29 21:20:03 -0300

    upgraded from netcoreapp3.1 to net8.0

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,26 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using AutoMapper;
+using CommandAPI.Data;
+using CommandAPI.Profiles;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace CommandAPI
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(CommandsProfile));
+
+builder.Services.AddDbContext<CommandContext>(opt => 
+    opt.UseNpgsql(config["Database:PostgreSqlConnection"]));
+    
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
